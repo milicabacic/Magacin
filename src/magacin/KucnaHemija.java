49e508ef62620cfa70dca221206b5df83136db81
milicabacic <milicabacic99@gmail.com>
2022-03-12 16:44:46 +0100

    Dodate klase Artikal, Knjiga, KancelarijskiMaterijal i KucnaHemija

---
+++ src/magacin/KucnaHemija.java
@@ -0,0 +1,17 @@
+package magacin;
+
+import java.util.Date;
+
+public class KucnaHemija extends Artikal {
+
+	private Date rokVazenja;
+
+	public Date getRokVazenja() {
+		return rokVazenja;
+	}
+
+	public void setRokVazenja(Date rokVazenja) {
+		this.rokVazenja = rokVazenja;
+	}
+	
+}
