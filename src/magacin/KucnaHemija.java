c4ee5df953f283dcf29b0e39c31d3a8ada961867
milicabacic <milicabacic99@gmail.com>
2022-03-13 17:59:50 +0100

    Izmenjene metode hashCode, equals i toString klasa Artikal i KucnaHemija

--- src/magacin/KucnaHemija.java
+++ src/magacin/KucnaHemija.java
@@ -1,6 +1,7 @@
 package magacin;
 
 import java.util.Date;
+import java.util.Objects;
 
 public class KucnaHemija extends Artikal {
 
@@ -13,5 +14,32 @@ public class KucnaHemija extends Artikal {
 	public void setRokVazenja(Date rokVazenja) {
 		this.rokVazenja = rokVazenja;
 	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = super.hashCode();
+		result = prime * result + Objects.hash(rokVazenja);
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (!super.equals(obj))
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		KucnaHemija other = (KucnaHemija) obj;
+		return Objects.equals(rokVazenja, other.rokVazenja);
+	}
+
+	@Override
+	public String toString() {
+		return super.toString() + "KucnaHemija [rokVazenja=" + rokVazenja + "]";
+	}
+	
+	
 	
 }
